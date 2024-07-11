7862a855e50cdcf9be6a4ff1fab995eb7d188eb3
Sanyok <33344827+sanyokdb@users.noreply.github.com>
2024-07-11 17:59:58 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Бургер
+// const burger = document.getElementById("burger");
+// const nav = document.getElementById("nav");
+
+// burger.addEventListener("click", function(){
+//   nav.classList.toggle("nav--active");
+//   burger.classList.toggle("burger--active");
+//   document.body.classList.toggle("no-scroll");
+// })
+
+const heroSlider = new Swiper(".hero-slider", {
+  loop: true,
+  effect: "fade",
+  autoplay: {
+    delay: 4500,
+  },
+  navigation: {
+    nextEl: ".hero-slider__navigation-next",
+    prevEl: ".hero-slider__navigation-prev",
+  },
+  pagination: {
+    el: ".hero-slider__pagination .number",
+    type: "fraction",
+    formatFractionCurrent: function (number) {
+      return ("0" + number).slice(-2);
+    },
+    formatFractionTotal: function (number) {
+      return ("0" + number).slice(-2);
+    }
+  },
+});
